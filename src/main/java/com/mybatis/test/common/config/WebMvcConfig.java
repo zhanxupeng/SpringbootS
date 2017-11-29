faf0e18ed03e616af10525f87b2f1299fc877199
zhanxupeng <1098481123@qq.com>
2017-11-29 22:41:55 +0800

    layui集成

---
+++ src/main/java/com/mybatis/test/common/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.mybatis.test.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by 占大帅 on 2017/11/28.
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //将所有/static/** 访问都映射到classpath:/static/ 目录下
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
---
+++ src/main/java/com/mybatis/test/controller/business/HelloController.java
@@ -0,0 +1,37 @@
+package com.mybatis.test.controller.business;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by 占大帅 on 2017/11/28.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String getIndex() {
+        return "index";
+    }
+
+    @RequestMapping("/{index}")
+    public String getUrl(@PathVariable("index") String index) {
+        return index;
+    }
+
+    @RequestMapping("/user/{index}")
+    public String getUser(@PathVariable("index") String index) {
+        return "user/" + index;
+    }
+
+    @RequestMapping("/jie/{index}")
+    public String getJie(@PathVariable("index") String index) {
+        return "jie/" + index;
+    }
+
+    @RequestMapping("/test/{index}")
+    public String getTest(@PathVariable("index") String index) {
+        return "test/" + index;
+    }
+}
