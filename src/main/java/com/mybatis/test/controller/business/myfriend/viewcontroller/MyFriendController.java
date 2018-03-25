f41a7c334b6ae6cd12db1eac5acbedd5cfc2a827
zhanxupeng <1098481123@qq.com>
2018-03-25 15:33:11 +0800

    亲密度代码优化

--- src/main/java/com/mybatis/test/controller/business/myfriend/viewcontroller/MyFriendController.java
+++ src/main/java/com/mybatis/test/controller/business/myfriend/viewcontroller/MyFriendController.java
@@ -5,23 +5,21 @@ import com.mybatis.test.controller.base.BaseController;
 import com.mybatis.test.controller.business.myfriend.viewmodel.MyFamiliarityFriendVM;
 import com.mybatis.test.domain.MyFamiliarityFriendInfo;
 import com.mybatis.test.service.api.myfriend.IMyFriendService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequestMapping("friend")
 public class MyFriendController extends BaseController {
     @Resource
     private IMyFriendService myFriendService;
 
     @GetMapping("familiarityRank")
-    @ResponseBody
     public Response familiarityRank() {
         List<MyFamiliarityFriendInfo> myFamiliarityFriendInfoList = myFriendService.getMyFamiliarityFriend();
         List<MyFamiliarityFriendVM> myFamiliarityFriendVMList = myFamiliarityFriendInfoList.stream().map(MyFamiliarityFriendVM::new)
